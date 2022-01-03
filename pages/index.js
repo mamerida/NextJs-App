6d2ad31da27f4108515e777fbfd26e2134b1d42c
Mario <mamerida2013@gmail.com>
2022-01-03 12:57:28 -0300

    trabajo con imagenes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Link from 'next/link'
+import Image from 'next/image'
+//de esta manera se importa directamente la imagen 
+import Coffee from '../public/coffee.png'
+
+//esta es la forma trabajando importando la imagen 
+//<Image src="/coffee.png" width={700} height={300} />
+
 
 export default function Home() {
   return (
   <div>
-    <p>Chanchito Feliz</p>
     <Link href="/chanchitos">Ir a Chanchitos</Link>
+    <p>Chanchito Feliz</p>
+    {/* al cargar imagen o ruta o importando la imagen con src widht height obligatorias o layout='fill' uitiliza todo el ancho y alto disponible  /<nombre imagen> obligatorio*/}
+    {/* al importante el componente le paso el objeto importado de la imagen  */}
+    <Image width={700} height={300} src={Coffee}/>
   </div>
   )
 }
